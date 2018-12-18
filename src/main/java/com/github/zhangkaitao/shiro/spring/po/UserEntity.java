90645d57106b4ddf867c25a579c0ddb463fe4c5f
林家小院 <guolinit@163.com>
2018-12-18 18:25:17 +0800

    'modify'

---
+++ src/main/java/com/github/zhangkaitao/shiro/spring/po/UserEntity.java
@@ -0,0 +1,156 @@
+package com.github.zhangkaitao.shiro.spring.po;
+
+import com.github.zhangkaitao.shiro.chapter22.entity.User;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <p>Title: UserEntity</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2018</p>
+ * <p>Company: www.hanshow.com</p>
+ *
+ * @author guolin
+ * @version 1.0
+ * @date 2018-12-18 18:06
+ */
+public class UserEntity implements Serializable {
+    private Long id; //编号
+    private Long organizationId; //所属公司
+    private String username; //用户名
+    private String password; //密码
+    private String salt; //加密密码的盐
+    private List<Long> roleIds; //拥有的角色列表
+    private Boolean locked = Boolean.FALSE;
+
+    public UserEntity() {
+    }
+
+    public UserEntity(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOrganizationId() {
+        return organizationId;
+    }
+
+    public void setOrganizationId(Long organizationId) {
+        this.organizationId = organizationId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getCredentialsSalt() {
+        return username + salt;
+    }
+
+    public List<Long> getRoleIds() {
+        if(roleIds == null) {
+            roleIds = new ArrayList<Long>();
+        }
+        return roleIds;
+    }
+
+    public void setRoleIds(List<Long> roleIds) {
+        this.roleIds = roleIds;
+    }
+
+
+    public String getRoleIdsStr() {
+        if(CollectionUtils.isEmpty(roleIds)) {
+            return "";
+        }
+        StringBuilder s = new StringBuilder();
+        for(Long roleId : roleIds) {
+            s.append(roleId);
+            s.append(",");
+        }
+        return s.toString();
+    }
+
+    public void setRoleIdsStr(String roleIdsStr) {
+        if(StringUtils.isEmpty(roleIdsStr)) {
+            return;
+        }
+        String[] roleIdStrs = roleIdsStr.split(",");
+        for(String roleIdStr : roleIdStrs) {
+            if(StringUtils.isEmpty(roleIdStr)) {
+                continue;
+            }
+            getRoleIds().add(Long.valueOf(roleIdStr));
+        }
+    }
+
+    public Boolean getLocked() {
+        return locked;
+    }
+
+    public void setLocked(Boolean locked) {
+        this.locked = locked;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != null ? !id.equals(user.getId()) : user.getId() != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", organizationId=" + organizationId +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", salt='" + salt + '\'' +
+                ", roleIds=" + roleIds +
+                ", locked=" + locked +
+                '}';
+    }
+}
