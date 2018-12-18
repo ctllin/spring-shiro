0a378a55247f5e3d172ab03b27d7ed2d5ec9301b
林家小院 <guolinit@163.com>
2018-12-18 15:10:55 +0800

    'init'

---
+++ src/main/java/com/github/zhangkaitao/shiro/chapter22/Constants.java
@@ -0,0 +1,10 @@
+package com.github.zhangkaitao.shiro.chapter22;
+
+/**
+ * <p>User: ctllin
+ * <p>Date: 14-2-15
+ * <p>Version: 1.0
+ */
+public class Constants {
+    public static final String CURRENT_USER = "user";
+}
---
+++ src/main/java/test/ShiroTest.java
@@ -0,0 +1,45 @@
+package test;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+//import org.junit.Assert;
+//import org.junit.Test;
+
+/**
+ * <p>Title: ShiroTest</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2018</p>
+ * <p>Company: www.hanshow.com</p>
+ *
+ * @author guolin
+ * @version 1.0
+ * @date 2018-12-18 10:23
+ */
+public class ShiroTest {
+
+
+    public static void main(String[] args) {
+        //1、获取 SecurityManager 工厂，此处使用 Ini 配置文件初始化 SecurityManager
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        //2、得到 SecurityManager 实例 并绑定给 SecurityUtils
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+        //3、得到 Subject 及创建用户名/密码身份验证 Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("admin", "liebe1");
+        try {
+            //4、登录，即身份验证
+            subject.login(token);
+        } catch (AuthenticationException e) {
+            //5、身份验证失败
+        }
+        //Assert.assertEquals(true, subject.isAuthenticated()); //断言用户已经登录
+        //6、退出
+        subject.logout();
+    }
+}
